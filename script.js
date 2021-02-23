5f6e7422482bf50ab79a9d1e0c50e9fb32287e00
NyarehD <nyarehdavid56@gmail.com>
2021-02-23 10:29:17 +0630

    Made the snake move

--- script.js
+++ script.js
@@ -1,18 +1,25 @@
 const canvas = document.getElementById("gameCanvas");
 const canvas_ctx = canvas.getContext("2d");
-let snake = [ {x: 200, y:200}, {x: 190, y: 200}, {x: 180, y: 200}, {x: 170, y:200 }];
-
+let snake = [ {x: 200, y:200}, {x: 190, y: 200}, {x: 180, y: 200}, {x: 170, y:200}];
+let dx = 10;
+let dy = 0;
 /* Colors */
 const border = 'black';
 const background = 'white';
-let snakeFill = 'lightBlue';
-let snakeStroke = 'darkBlue';
+const snakeFill = 'lightBlue';
+const snakeStroke = 'darkBlue';
 
+document.addEventListener("keydown", changeDirection);
 
 main();
+/* Make the canvas clean first and then create snake parts */
 function main(){
-  drawSnake();
-
+  setTimeout(function onTick(){
+    clearCanvas();  
+    drawSnake();
+    moving();
+    main();
+  }, 100);
 }
 /* Clearing canvas */
 function clearCanvas(){
@@ -33,4 +40,38 @@ function drawingSnakeParts(snakeParts){
 
 function drawSnake(){
     snake.forEach(drawingSnakeParts);
+}
+
+/* functin for moving x-axis */
+function moving(){
+  let head = {x: snake[0].x+dx, y: snake[0].y+dy};
+  snake.unshift(head);
+  snake.pop();
+}
+
+function changeDirection(event){
+  const leftKey = 37;
+  const rightKey = 39;
+  const upKey = 38;
+  const downKey = 40;
+
+  let keyPressed = event.keyCode;
+  let goingDown = dy === 10;
+  let goingUp = dy === -10;
+  let goingRight = dx === 10;
+  let goingLeft = dx === -10;
+  
+  if (leftKey === keyPressed && !goingRight){
+    dx = -10;
+    dy = 0;
+  }else if (rightKey === keyPressed && !goingLeft){
+    dx = 10;
+    dy = 0;
+  }else if (upKey === keyPressed && !goingDown){
+    dx = 0;
+    dy = -10;
+  }else if (downKey === keyPressed && !goingUp){
+    dx = 0;
+    dy = 10;
+  }
 }
